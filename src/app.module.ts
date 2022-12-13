8de91be546eeccd01c0ab6901f5a81044a2fbbec
scw3812 <scw3812@naver.com>
2022-12-13 09:29:16 +0900

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { BoardsModule } from './boards/boards.module';
+import { BoardsController } from './boards/boards.controller';
+
+@Module({
+  imports: [BoardsModule],
+  controllers: [BoardsController],
+  providers: [],
+})
+export class AppModule {}
